1098dc15a3bf15555de4ef16af7edb7a4aa6e373
jk2K <andy1945jp@gmail.com>
2015-09-01 18:12:58 +0800

    增加图片选择控件

--- app/src/main/java/com/jk2k/helloworld/MainActivity.java
+++ app/src/main/java/com/jk2k/helloworld/MainActivity.java
@@ -1,14 +1,49 @@
 package com.jk2k.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import me.iwf.photopicker.PhotoPickerActivity;
+import me.iwf.photopicker.utils.PhotoPickerIntent;
 
 public class MainActivity extends AppCompatActivity {
+    public final static int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.selectPhotoButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
+                intent.setPhotoCount(1);
+                intent.setShowCamera(true);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    public void previewPhoto(Intent intent) {
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        ArrayList<String> photos = null;
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data != null) {
+                photos = data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
+                Log.i("test", photos.toString());
+            }
+        }
     }
 }
